3a2ad2d6feed7218e0361328a8e1b4b502815556
Atsushi Hirata <atsushi.hirata890@gmail.com>
2020-11-04 00:09:42 +0900

    rename file to properly reflect operation

---
+++ setup_dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# setup nvim
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+ln -sf ~/workspace/dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/workspace/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
+
+# setup vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# nodebrew directory
+mkdir -p ~/.nodebrew/src
