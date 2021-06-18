a8e2f2c617160853ccc8d6478df58c85481bd08f
Atsushi Hirata <atsushi.hirata890@gmail.com>
2021-06-18 13:40:38 +0900

    add alacritty symlink

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -6,11 +6,14 @@ ln -sf ~/workspace/dotfiles/.zshrc ~/.zshrc
 # setup vi
 ln -sf ~/workspace/dotfiles/.exrc ~/.exrc
 
+# setup alacritty
+mkdir -p ~/.config/alacritty
+ln -sf ~/workspace/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+
 # setup nvim
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
 ln -sf ~/workspace/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sf ~/workspace/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # setup vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
